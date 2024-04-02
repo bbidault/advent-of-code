01e19a55684b09dd97adc89970201c6e2a7eb14e
bbidault <bbidault@dekaresearch.com>
2024-04-02 16:12:28 -0400

    Cleanup 2023 day 9

--- 2023/day9/day9.py
+++ 2023/day9/day9.py
@@ -1,30 +1,53 @@
+# https://adventofcode.com/2023/day/9
 
-inputFile = open("input.txt", "r")
-Lines = inputFile.readlines()
-sum = 0
-for line in Lines:
-   table = []
-   numbers = line.split(" ")
-   subtable = []
-   for i in range(len(numbers) - 1, -1, -1):
-      subtable.append(int(numbers[i]))
-   table.append(subtable)
-   allZeros = False
-   idx = 0
-   while False == allZeros:
-      idx += 1
+# Calculate and return the sum of predicted numbers
+#
+# @param inputFilePath: the input file
+# @param part1: whether we should predict the future (part 1, True) or the past (part 2, False)
+# @return: the sum of predicted numbers
+#
+def solve(inputFilePath: str, part1: bool) -> int:
+   sum = 0
+
+   inputFile = open(inputFilePath, "r")
+   lines = inputFile.readlines()
+   inputFile.close()
+   for line in lines:
+      # ingest the input into a table of list of numbers
+      numbers = line.split(" ")
       subtable = []
-      for i in range(len(table[idx - 1]) - 1):
-         subtable.append(table[idx - 1][i + 1] - table[idx - 1][i])
-      table.append(subtable)
-      allZeros = True
-      for i in range(len(table[idx])):
-         if table[idx][i] != 0:
-            allZeros = False
-            break
-   for i in range(len(table) - 2, -1, -1):
-      table[i].append(table[i][len(table[i]) - 1] + table[i + 1][len(table[i + 1]) - 1])
-   print(str(table[0][len(table[0]) - 1]))
-   sum += table[0][len(table[0]) - 1]
-print(sum)
-inputFile.close()
+      if part1:
+         for num in numbers:
+            subtable.append(int(num))
+      else:
+         for jdx in range(len(numbers) - 1, -1, -1):  # prepare the initial list of numbers in reverse order for part 2
+            subtable.append(int(numbers[jdx]))
+      table = [subtable]
+
+      # generate each consecutive history rows of the table until we have a row containing only zeros
+      allZeros = False
+      idx = 0
+      while not allZeros:
+         idx += 1
+         subtable = []
+         for jdx in range(len(table[idx - 1]) - 1):
+            subtable.append(table[idx - 1][jdx + 1] - table[idx - 1][jdx])  # calculate the history for the current row
+         table.append(subtable)
+         allZeros = True
+         for num in table[idx]:
+            if num != 0:
+               allZeros = False
+               break
+
+      # generate the prediction for each row
+      for jdx in range(len(table) - 2, -1, -1):
+         table[jdx].append(table[jdx][len(table[jdx]) - 1] + table[jdx + 1][len(table[jdx + 1]) - 1])
+
+      sum += table[0][-1]
+   return sum
+
+
+print("Part 1 test:", solve("test_input.txt", True))
+print("Part 1 solution:", solve("input.txt", True))
+print("Part 2 test:", solve("test_input.txt", False))
+print("Part 2 solution:", solve("input.txt", False))
