9b542520ce90e8097430dcb7427ffc54187905dc
bbidault <bbidault@dekaresearch.com>
2024-04-03 14:40:18 -0400

    Add comments for 2023 day 12

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -9,19 +9,19 @@ import functools
 # @param splits: the pattern defined as numbers of # seperated by .
 # @return: the number of patterns matching the given parameters
 #
-@functools.lru_cache(maxsize=None)
+@functools.lru_cache(maxsize=None)  # use memoization for performance
 def findpatterns(pattern: list, splits: tuple) -> int:
    count = 0
-   patternMaxStart = len(pattern) - sum(splits) - len(splits[1:]) + 1
-   for idx in range(patternMaxStart):
-      if all(char in "#?" for char in pattern[idx:idx + splits[0]]):
-         if len(splits) == 1:
-            if all(char in ".?" for char in pattern[idx + splits[0]: len(pattern)]):
-               count += 1
-         elif pattern[idx + splits[0]] in '.?':
-            count += findpatterns(pattern[idx + splits[0] + 1:], splits[1:])
-      if pattern[idx] not in '.?':
-         break
+   patternMaxStart = len(pattern) - sum(splits) - len(splits[1:])  # lenght of pattern - minimum length of pattern
+   for idx in range(patternMaxStart + 1):  # + 1 buffer for range
+      if all(char in "#?" for char in pattern[idx:idx + splits[0]]):  # if we can fit the full split (all can be #)
+         if len(splits) == 1:  # if this is the last split to fit
+            if all(char in ".?" for char in pattern[idx + splits[0]: len(pattern)]):  # if everything left in the pattern can be a .
+               count += 1  # we have a matching pattern
+         elif pattern[idx + splits[0]] in '.?':  # if we can have a . after the split
+            count += findpatterns(pattern[idx + splits[0] + 1:], splits[1:])  # recurse on the remaining pattern and splits
+      if pattern[idx] not in '.?':  # if we cannot fit the split in the pattern
+         break  # we are done
    return count
 
 
