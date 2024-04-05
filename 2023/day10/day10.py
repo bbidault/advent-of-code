50f61f7a4003be77dbb38db768bc61e8b0a233ac
bbidault <bbidault@dekaresearch.com>
2024-04-05 16:55:07 -0400

    Cleanup 2023 day 10

--- 2023/day10/day10.py
+++ 2023/day10/day10.py
@@ -1,72 +1,66 @@
+# https://adventofcode.com/2023/day/10
 
+# ingest the input file
 inputFile = open("input.txt", "r")
-Lines = inputFile.readlines()
-table = []
-display = []
-start = [0, 0]
-for i in range(len(Lines)):
-   subtable = []
-   subDisplay = []
-   for j in range(len(Lines[i])):
-      subtable.append(Lines[i][j])
-      subDisplay.append(" ")
-      if Lines[i][j] == "S":
-         start = [i, j]
-   table.append(subtable)
-   display.append(subDisplay)
-display[start[0]][start[1]] = "#"
+lines = inputFile.readlines()
+inputFile.close()
+table = []  # the input in a table of character
+pipe = []  # a table that capture main pipe element in a table of booleans, Pipe = true, no pipe = false
+start = (0, 0)  # the "start" of the pipe
+for row in range(len(lines)):
+   subTable = []
+   subPipe = []
+   for col in range(len(lines[row])):
+      subTable.append(lines[row][col])
+      subPipe.append(False)
+      if lines[row][col] == "S":
+         start = (row, col)
+   table.append(subTable)
+   pipe.append(subPipe)
+pipe[start[0]][start[1]] = True
+
+# part 1, walk and measure the length of the loop from the start
 origin = start
-location = [start[0] + 1, start[1]]
+location = (start[0] + 1, start[1])  # we know by looking at the input that the cell bellow the start is on the pipe
 length = 1
 while table[location[0]][location[1]] != "S":
-   display[location[0]][location[1]] = "#"
-   candidate1 = [0, 0]
-   candidate2 = [0, 0]
+   pipe[location[0]][location[1]] = True
+   candidates = ((0, 0), (0, 0))
    if table[location[0]][location[1]] == "|":
-      candidate1 = [location[0] - 1, location[1]]
-      candidate2 = [location[0] + 1, location[1]]
+      candidates = ((location[0] - 1, location[1]), (location[0] + 1, location[1]))
    elif table[location[0]][location[1]] == "-":
-      candidate1 = [location[0], location[1] - 1]
-      candidate2 = [location[0], location[1] + 1]
+      candidates = ((location[0], location[1] - 1), (location[0], location[1] + 1))
    elif table[location[0]][location[1]] == "L":
-      candidate1 = [location[0] - 1, location[1]]
-      candidate2 = [location[0], location[1] + 1]
+      candidates = ((location[0] - 1, location[1]), (location[0], location[1] + 1))
    elif table[location[0]][location[1]] == "J":
-      candidate1 = [location[0] - 1, location[1]]
-      candidate2 = [location[0], location[1] - 1]
+      candidates = ((location[0] - 1, location[1]), (location[0], location[1] - 1))
    elif table[location[0]][location[1]] == "7":
-      candidate1 = [location[0], location[1] - 1]
-      candidate2 = [location[0] + 1, location[1]]
+      candidates = ((location[0], location[1] - 1), (location[0] + 1, location[1]))
    elif table[location[0]][location[1]] == "F":
-      candidate1 = [location[0], location[1] + 1]
-      candidate2 = [location[0] + 1, location[1]]
-   if candidate1 == origin:
+      candidates = ((location[0], location[1] + 1), (location[0] + 1, location[1]))
+   if candidates[0] == origin:
       origin = location
-      location = candidate2
+      location = candidates[1]
    else:
       origin = location
-      location = candidate1
+      location = candidates[0]
    length += 1
-print(str(length / 2))
 
+print("Part 1 solution:", str(length / 2))  # the distance to the farthest point from the start is half the length of the full loop
 
+# part 2, calculate the sum of cells inside the path loop
 sum = 0
-for i in range(len(display)):
-   # print(str(len(table[i])) + " " + str(len(display[i])))
-   out = True
-   for j in range(len(display[i])):
-      if display[i][j] == "#":
-         if (table[i][j] == "|") or (table[i][j] == "7") or (table[i][j] == "F") or (table[i][j] == "S"):
+for idx in range(len(pipe)):
+   out = True  # outside of the loop
+   for jdx in range(len(pipe[idx])):
+      if pipe[idx][jdx]:
+         if ((table[idx][jdx] == "|") or
+                 (table[idx][jdx] == "7") or
+                 (table[idx][jdx] == "F") or
+                 (table[idx][jdx] == "S")):  # The start pipe is 7 shaped
             out = not out
       else:
-         if (False == out):
+         if not out:
             sum += 1
-            display[i][j] = "I"
-print(str(sum))
 
-for i in range(len(display)):
-   for j in range(len(display[i])):
-      print(display[i][j], end="")
-   print()
-
-inputFile.close()
+print("Part 2 solution:", str(sum))
