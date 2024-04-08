8768b5860c8b6908ad5f08efeb3d8194e0fca650
bbidault <bbidault@dekaresearch.com>
2024-04-08 16:15:41 -0400

    2023 day 13

--- 2023/day13/day13.py
+++ 2023/day13/day13.py
@@ -1,124 +1,135 @@
+# https://adventofcode.com/2023/day/13
 
-import math
 
-inputFile = open("input.txt", "r")
-Lines = inputFile.readlines()
-answer = 0
-table = []
-count = 0
-for line in Lines:
-   if line.strip():
-      subtable = []
-      for char in line:
-         if char in ".#":
-            subtable.append(char)
-      table.append(subtable)
-   else:
-      # print(table)
+# Find the symmetry in a list of strings
+#
+# @param list: a list of strings
+# @return: the index of the symmetry or 0 if none
+#
+def findSymmetryInList(list: list) -> tuple:
+   for i in range(len(list) - 1):
+      symmetry = True
+      idx = 0
+      while i - idx >= 0 and i + 1 + idx < len(list):
+         if list[i - idx] != list[i + 1 + idx]:
+            symmetry = False
+            break
+         idx += 1
+      if symmetry:
+         return (i + 1)
+   return 0  # no symmetry
 
-      vertical = -1
-      horizontal = -1
-      decFromBin = []
-      for i in range(len(table)):
-         decFromBin.append(0)
-         for j in range(len(table[i])):
-            # print(str(table[i][j]) + " ", end='')
-            if table[i][j] == "#":
-               # print("sum: " + str(decFromBin[i]), end='' )
-               decFromBin[i] += math.pow(2, j)
-               # print(" j: " + str(j) + " sum: " + str(decFromBin[i]) + ", ", end='')
-      # print(decFromBin)
-      for i in range(len(decFromBin) - 1):
-         symmetry = True
-         idx = 0
-         while i - idx >= 0 and i + 1 + idx < len(decFromBin):
-            if decFromBin[i - idx] != decFromBin[i + 1 + idx]:
-               symmetry = False
-               break
-            idx += 1
-         if symmetry:
-            # print("table " + str(count) + " horizontal symmetry at " + str(i + 1))
-            horizontal = i + 1
-      decFromBin = []
-      for i in range(len(table[0])):
-         decFromBin.append(0)
-         for j in range(len(table)):
-            if table[j][i] == "#":
-               decFromBin[i] += math.pow(2, j)
-      # print(decFromBin)
-      for i in range(len(decFromBin) - 1):
-         symmetry = True
-         idx = 0
-         while i - idx >= 0 and i + 1 + idx < len(decFromBin):
-            if decFromBin[i - idx] != decFromBin[i + 1 + idx]:
-               symmetry = False
-               break
-            idx += 1
-         if symmetry:
-            # print("table " + str(count) + " vertical symmetry at " + str(i + 1))
-            vertical = i + 1
 
-      for k in range(len(table)):
-         for l in range(len(table[0])):
-            table[k][l] = "." if table[k][l] == "#" else "#"
-            decFromBin = []
-            for i in range(len(table)):
-               decFromBin.append(0)
-               for j in range(len(table[i])):
-                  # print(str(table[i][j]) + " ", end='')
-                  if table[i][j] == "#":
-                     # print("sum: " + str(decFromBin[i]), end='' )
-                     decFromBin[i] += math.pow(2, j)
-                     # print(" j: " + str(j) + " sum: " + str(decFromBin[i]) + ", ", end='')
-            # print(decFromBin)
-            for i in range(len(decFromBin) - 1):
-               symmetry = True
-               idx = 0
-               while i - idx >= 0 and i + 1 + idx < len(decFromBin):
-                  if decFromBin[i - idx] != decFromBin[i + 1 + idx]:
-                     symmetry = False
-                     break
-                  idx += 1
-               if symmetry:
-                  if horizontal != i + 1:
-                     print("table " + str(count) + " horizontal symmetry at " + str(i + 1))
-                     answer += (i + 1) * 100
-            decFromBin = []
-            for i in range(len(table[0])):
-               decFromBin.append(0)
-               for j in range(len(table)):
-                  if table[j][i] == "#":
-                     decFromBin[i] += math.pow(2, j)
-            # print(decFromBin)
-            for i in range(len(decFromBin) - 1):
-               symmetry = True
-               idx = 0
-               while i - idx >= 0 and i + 1 + idx < len(decFromBin):
-                  if decFromBin[i - idx] != decFromBin[i + 1 + idx]:
-                     symmetry = False
-                     break
-                  idx += 1
-               if symmetry:
-                  if vertical != i + 1:
-                     print("table " + str(count) + " vertical symmetry at " + str(i + 1))
-                     answer += (i + 1)
+# Find the symmetry in a table of characters
+#
+# @param table: a table of characters
+# @return: the indexes of the symmetries or zeros if there are none
+#
+def findSymmetryInTable(table: list) -> tuple:
+   horizontal = findSymmetryInList([row for row in table])
+   rotated = list(zip(*table[::-1]))  # rotate the table to read the columns as rows
+   vertical = findSymmetryInList([col for col in rotated])
+   return (horizontal, vertical)
 
-            # if vertical > 0 or horizontal > 0:
-            #     for i in range(len(table)):
-            #         if horizontal == i:
-            #             for j in range(len(table[0])):
-            #                 print("-", end="")
-            #             print()
-            #         for j in range(len(table[0])):
-            #             if vertical == j:
-            #                 print("|", end="")
-            #             print(table[i][j], end="")
-            #         print()
-            #     print()
-            table[k][l] = "." if table[k][l] == "#" else "#"
 
-      table = []
-      count += 1
+# Calculate and return the sum of the locations of the symmetries
+#
+# @param inputFilePath: the input file
+# @return: the sum of the locations of the symmetries (time 100 if horizontal)
+#
+def part1(inputFilePath: str) -> int:
+   inputFile = open(inputFilePath, "r")
+   lines = inputFile.readlines()
+   inputFile.close()
+   answer = 0
+   table = []
+   for line in lines:
+      if line.strip():
+         subtable = []
+         for char in line:
+            if char in ".#":
+               subtable.append(char)
+         table.append(subtable)
+      else:
+         horizontal, vertical = findSymmetryInTable(table)
+         answer += horizontal * 100 + vertical
+         table = []
+   return answer
 
-print(answer)
-inputFile.close()
+
+# Find a new symmetry in a list of strings
+#
+# @param list: a list of strings
+# @param knownSymmetry: a known symmetry
+# @return: the index of the symmetry or 0 if no new one
+#
+def findNewSymmetry(list: list, knownSymmetry: int) -> int:
+   for i in range(len(list) - 1):
+      symmetry = True
+      idx = 0
+      while i - idx >= 0 and i + 1 + idx < len(list):
+         if list[i - idx] != list[i + 1 + idx]:
+            symmetry = False
+            break
+         idx += 1
+      if symmetry and knownSymmetry != i + 1:
+         return (i + 1)
+   return 0  # no symmetry
+
+
+# Identify and return the location of the new symmetry after clearing the "smudge"
+#
+# @param table: a table of characters
+# @return: the index of the new symmetry (time 100 if horizontal)
+#
+def findSmudge(table: list) -> int:  # rename
+   horizontal, vertical = findSymmetryInTable(table)
+   for i in range(len(table)):
+      for j in range(len(table[0])):
+         # for each cell in the table, switch its value
+         table[i][j] = "." if table[i][j] == "#" else "#"
+
+         rowAsString = [row for row in table]
+
+         newHorizontal = findNewSymmetry(rowAsString, horizontal)
+
+         rotated = list(zip(*table[::-1]))  # rotate the table to read the columns as rows
+         colAsString = [col for col in rotated]
+
+         newVertical = findNewSymmetry(colAsString, vertical)
+
+         table[i][j] = "." if table[i][j] == "#" else "#"  # revert the change
+
+         if newHorizontal != 0 or newVertical != 0:
+            return newHorizontal * 100 + newVertical
+
+
+# Calculate and return the sum of the locations of the new symmetries after clearing the "smudges"
+#
+# @param inputFilePath: the input file
+# @return: the sum of the locations of the new symmetries (time 100 if horizontal)
+#
+def part2(inputFilePath: str) -> int:
+   inputFile = open(inputFilePath, "r")
+   lines = inputFile.readlines()
+   inputFile.close()
+   answer = 0
+   table = []
+   for line in lines:
+      if line.strip():
+         subtable = []
+         for char in line:
+            if char in ".#":
+               subtable.append(char)
+         table.append(subtable)
+      else:
+         answer += findSmudge(table)
+         table = []
+
+   return answer
+
+
+print("Part 1 test:", part1("test_input.txt"))
+print("Part 1 solution:", part1("input.txt"))
+print("Part 2 test:", part2("test_input.txt"))
+print("Part 2 solution:", part2("input.txt"))
