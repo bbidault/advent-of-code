ccae7701fae9bea63d59e2814b111006c0ba9e61
bbidault <bbidault@dekaresearch.com>
2024-04-19 15:45:42 -0400

    Cleanup 2023 day 14 & 15

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -1,110 +1,107 @@
-def evaluate(table):
-   answer = 0
-   for i in range(len(table[0])):
-      for j in range(len(table)):
-         if table[j][i] == "O":
-            answer += len(table) - j
-   return answer
+# https://adventofcode.com/2023/day/14
 
 
-def tiltNorth(table):
-   newTable = [["."] * len(table[0]) for i in range(len(table))]
-   for i in range(len(table[0])):
+# Evaluates the load applied to the north support beam
+# The load is the sum of the distances of rounded rocks (O) to the north side of the table
+#
+# @param table: a table representing the arrangement of the rocks
+# @return: the load applied to the north support beam
+#
+def evaluate(table: list) -> int:
+   load = 0
+   for col in range(len(table[0])):
+      for row in range(len(table)):
+         if table[row][col] == "O":
+            load += len(table) - row
+   return load
+
+
+# Move the rounded rocks (O) as far north as possible until they reach the edge of the table or a static rock (#)
+#
+# @param table: a table representing the arrangement of the rocks
+# @return: the re-arranged table
+#
+def tiltNorth(table: list) -> list:
+   newTable = [["."] * len(table[0]) for _ in range(len(table))]
+   for col in range(len(table[0])):
       wall = 0
-      for j in range(len(table)):
-         if table[j][i] == "O":
-            newTable[wall][i] = "O"
+      for row in range(len(table)):
+         if table[row][col] == "O":
+            newTable[wall][col] = "O"
             wall += 1
-         elif table[j][i] == "#":
-            newTable[j][i] = "#"
-            wall = j + 1
+         elif table[row][col] == "#":
+            newTable[row][col] = "#"
+            wall = row + 1
    return newTable
 
 
-def tiltSouth(table):
-   newTable = [["."] * len(table[0]) for i in range(len(table))]
-   for i in range(len(table[0])):
-      wall = len(table) - 1
-      for j in range(len(table) - 1, -1, -1):
-         if table[j][i] == "O":
-            newTable[wall][i] = "O"
-            wall -= 1
-         elif table[j][i] == "#":
-            newTable[j][i] = "#"
-            wall = j - 1
-   return newTable
+# Ingest the input file and return a table representing the arrangement of rocks (O and #)
+#
+# @param inputFilePath: the input file
+# @return: a table representing the arrangement of rocks
+#
+def ingest(inputFilePath: str) -> list:
+   inputFile = open(inputFilePath, "r")
+   lines = inputFile.readlines()
+   inputFile.close()
+   table = []
+   for line in lines:
+      if line.strip():
+         subtable = []
+         for char in line:
+            if char in ".#O":
+               subtable.append(char)
+         table.append(subtable)
+   return table
 
 
-def tiltWest(table):
-   newTable = [["."] * len(table[0]) for i in range(len(table))]
-   for i in range(len(table)):
-      wall = 0
-      for j in range(len(table[0])):
-         if table[i][j] == "O":
-            newTable[i][wall] = "O"
-            wall += 1
-         elif table[i][j] == "#":
-            newTable[i][j] = "#"
-            wall = j + 1
-   return newTable
-
-
-def tiltEast(table):
-   newTable = [["."] * len(table[0]) for i in range(len(table))]
-   for i in range(len(table)):
-      wall = len(table[0]) - 1
-      for j in range(len(table[0]) - 1, -1, -1):
-         if table[i][j] == "O":
-            newTable[i][wall] = "O"
-            wall -= 1
-         elif table[i][j] == "#":
-            newTable[i][j] = "#"
-            wall = j - 1
-   return newTable
-
-
-inputFile = open("input.txt", "r")
-Lines = inputFile.readlines()
-answer = 0
-table = []
-count = 0
-for line in Lines:
-   if line.strip():
-      subtable = []
-      for char in line:
-         if char in ".#O":
-            subtable.append(char)
-      table.append(subtable)
-
-
-oldTableTiltedNorth = tiltNorth(table)
-combinations = []
-cycles = []
-for i in range(250000000):
-   test = False
+# Calculate and return the load on the north support beam after tilting the platform toward the north
+#
+# @param inputFilePath: the input file
+# @return: the load on the north support beam after tilting the platform toward the north
+#
+def part1(inputFilePath: str) -> int:
+   table = ingest(inputFilePath)
    table = tiltNorth(table)
-   if table in combinations:
-      print("cycle " + str(i) + " " + str(cycles[combinations.index(table)]))
-      test = True
-   else:
-      combinations.append(table)
-      cycles.append(i)
-   table = tiltWest(table)
-   table = tiltSouth(table)
-   table = tiltEast(table)
-   if test:
-      print(evaluate(table))
+   return evaluate(table)
 
 
-for i in range(len(table)):
-   for j in range(len(table[0])):
-      print(table[i][j], end="")
-   print()
+# Calculate and return the load on the north support beam after tilting the platform 1000000000 times in a north, west, south, east pattern
+#
+# @param inputFilePath: the input file
+# @return: the load on the north support beam after tilting the platform 1000000000 times in a north, west, south, east pattern
+#
+def part2(inputFilePath: str) -> int:
+   table = ingest(inputFilePath)
+   seenGrids = []
+   grids = []
 
-# 98890 - 98898
+   table = tiltNorth(table)
+   tableStr = str.join("", [str.join("", k) for k in table])  # hashable type
+   seenGrids.append(tableStr)
+   grids.append(table)
+   for cycle in range(1000000000):
+      table = tiltNorth(table)
+      table = list(zip(*table[::-1]))  # rotate
+      table = tiltNorth(table)
+      table = list(zip(*table[::-1]))  # rotate
+      table = tiltNorth(table)
+      table = list(zip(*table[::-1]))  # rotate
+      table = tiltNorth(table)
+      table = list(zip(*table[::-1]))  # rotate
 
-98893
-98894
+      tableStr = str.join("", [str.join("", k) for k in table])
+      if tableStr in seenGrids:
+         break
+      seenGrids.append(tableStr)
+      grids.append(table)
 
-print(evaluate(table))
-inputFile.close()
+   firstCycleGrid_idx = seenGrids.index(tableStr)
+   finalGrid = grids[(1000000000 - firstCycleGrid_idx) % (cycle + 1 - firstCycleGrid_idx) + firstCycleGrid_idx]
+   return evaluate(finalGrid)
+
+
+print("Part 1 test:", part1("test_input.txt"))
+print("Part 1 solution:", part1("input.txt"))
+print("Part 2 test:", part2("test_input.txt"))
+print("Part 2 solution:", part2("input.txt"))
